f6f9d14b610777697f678f2e0899858e2a882b64
Sadra Samadi <sadra.smd@gmail.com>
2020-09-22 10:05:28 +0330

    first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
+import util from 'util';
+
+const port = parseInt(process.env.PORT) || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/info', (req, res) => {
+  res.json({
+    name: 'Sadra Samadi',
+    title: 'Full-stack Developer',
+    email: 'sadra.smd@gmail.com',
+    phone: '+98 902 128 6464',
+    telegram: '@SadraSamadi'
+  });
+});
+
+(async () => {
+
+  let listen = server.listen.bind(server);
+  await util.promisify(listen)(port);
+  console.log(`server started on port ${port}`);
+
+})();
